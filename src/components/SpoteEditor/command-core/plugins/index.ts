7e7692da564dc6379d60993fdd434d55842bfcf0
Anders Bergkvist <andersbergkvist@hotmail.com>
2026-09-02 20:29:49 +0200

    feat: add a /mermaid slash command

--- src/components/SpoteEditor/command-core/plugins/index.ts
+++ src/components/SpoteEditor/command-core/plugins/index.ts
@@ -4,10 +4,10 @@ export * from './blocks'
 export * from './image'
 import { bold, italic, code } from './marks'
 import { link } from './link'
-import { h1, h2, h3, bulletList, orderedList, quote, codeBlock, divider } from './blocks'
+import { h1, h2, h3, bulletList, orderedList, quote, codeBlock, mermaid, divider } from './blocks'
 import { image } from './image'
 import type { SpotePlugin } from '../plugin.types'
 
 export const DEFAULT_PLUGINS: SpotePlugin[] = [
-  h1, h2, h3, bold, italic, code, codeBlock, bulletList, orderedList, quote, link, image, divider,
+  h1, h2, h3, bold, italic, code, codeBlock, mermaid, bulletList, orderedList, quote, link, image, divider,
 ]
